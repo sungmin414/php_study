c5cd161fc597655b9d313a9c8707095529954d60
sungmin414 <sungmin3231@gmail.com>
2020-04-27 18:18:24 +0900

    조건문 활용

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <?php
         echo date('Y-m-d H:i:s');
     ?>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
@@ -18,11 +18,21 @@
     </ol>
     <h2>
         <?php
+        if (isset($_GET['id'])){
             echo $_GET['id'];
+        } else {
+            echo "Welcom";
+        }
+
         ?>
     </h2>
     <?php
-    echo file_get_contents("data/".$_GET['id']);
+    if(isset($_GET['id'])){
+        echo file_get_contents("data/".$_GET['id']);
+    } else {
+        echo 'Hello, PHP';
+    }
+
     ?>
 </body>
 </html>
