1fbfce6bae6309fee5a638f31de5c0e62da1860d
sungmin414 <sungmin3231@gmail.com>
2020-04-28 20:25:26 +0900

    form과 post를 사용해서 글 생성,수정,삭제 구현, 파일 모듈화, 보안 구현

--- index.php
+++ index.php
@@ -1,64 +1,16 @@
 <?php
-function print_title(){
-    if (isset($_GET['id'])){
-        echo $_GET['id'];
-    } else {
-        echo "Welcom";
-    }
-}
-function print_description(){
-    if(isset($_GET['id'])){
-        echo file_get_contents("data/".$_GET['id']);
-    } else {
-        echo 'Hello, PHP';
-    }
-}
-function print_list(){
-    $list = scandir('./data');
-
-    $i = 0;
-    while ($i < count($list)) {
-        if ($list[$i] != '.'){
-            if ($list[$i] != '..'){
-        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-        }
-        $i = $i + 1;
-
-    }
-
-    /*
-    echo "<li>$list[0]</li>\n";
-    echo "<li>$list[1]</li>\n";
-    echo "<li>$list[2]</li>\n";
-    echo "<li>$list[3]</li>\n";
-    echo "<li>$list[4]</li>\n";
-    echo "<li>$list[5]</li>\n";
-    */
-}
+    require_once('lib/print.php');
+    require_once('view/top.php')
 ?>
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>
-        <?php
-        print_title();
-        ?>
-    </title>
-</head>
-<body>
-    <?php
-        echo date('Y-m-d H:i:s');
-    ?>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-    <?php
-        print_list();
-    ?>
-    </ol>
+    <a href="create.php">create</a>
+    <?php if(isset($_GET['id'])) { ?>
+    <a href="update.php?id=<?=$_GET['id']?>">update</a>
+    <form action="delete_process.php" method="post">
+        <input type="hidden" name="id" value="<?=$_GET['id']?>">
+        <input type="submit" value="delete">
+    </form>
+
+    <?php } ?>
     <h2>
         <?php
         print_title();
@@ -67,5 +19,6 @@ function print_list(){
     <?php
         print_description();
     ?>
-</body>
-</html>
+<?php
+ require_once('view/bottom.php');
+?>
---
+++ view/top.php
@@ -0,0 +1,25 @@
+<?php
+require_once('lib/print.php')
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php
+        print_title();
+        ?>
+    </title>
+</head>
+<body>
+    <?php
+        echo date('Y-m-d H:i:s');
+    ?>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+    <?php
+        print_list();
+    ?>
+    </ol>
