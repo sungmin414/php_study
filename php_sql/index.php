172ac419a9fcd0f69040f320e1129e995dc26f1b
sungmin414 <sungmin3231@gmail.com>
2020-04-28 22:10:17 +0900

    sql 사용전 준비

---
+++ php_sql/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <h2>Welcom</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+</body>
+</html>
