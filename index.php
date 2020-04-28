ba025c216c22243875e8d714fba3c039ef44e726
sungmin414 <sungmin3231@gmail.com>
2020-04-28 12:37:24 +0900

    반복문과 배열의 활용

--- index.php
+++ index.php
@@ -13,18 +13,27 @@
     <h1><a href="index.php">WEB</a></h1>
     <ol>
     <?php
-    $list = scandir('data');
-    $i = 0;
-    while ($i<count($list)) {
-        if($list[$i] != '.'){
-            if($list[$i] != '..'){
-                ?>
-                <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
-                <?php
+        $list = scandir('./data');
+
+        $i = 0;
+        while ($i < count($list)) {
+            if ($list[$i] != '.'){
+                if ($list[$i] != '..'){
+            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
             }
+            $i = $i + 1;
+
         }
-        $i = $i + 1;
-    }
+
+        /*
+        echo "<li>$list[0]</li>\n";
+        echo "<li>$list[1]</li>\n";
+        echo "<li>$list[2]</li>\n";
+        echo "<li>$list[3]</li>\n";
+        echo "<li>$list[4]</li>\n";
+        echo "<li>$list[5]</li>\n";
+        */
     ?>
     </ol>
     <h2>
