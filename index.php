095dfcaf05c1e90d4b81855734260c9e2b5cd355
sungmin414 <sungmin3231@gmail.com>
2020-04-28 15:06:06 +0900

    함수의 활용

--- index.php
+++ index.php
@@ -1,10 +1,53 @@
+<?php
+function print_title(){
+    if (isset($_GET['id'])){
+        echo $_GET['id'];
+    } else {
+        echo "Welcom";
+    }
+}
+function print_description(){
+    if(isset($_GET['id'])){
+        echo file_get_contents("data/".$_GET['id']);
+    } else {
+        echo 'Hello, PHP';
+    }
+}
+function print_list(){
+    $list = scandir('./data');
+
+    $i = 0;
+    while ($i < count($list)) {
+        if ($list[$i] != '.'){
+            if ($list[$i] != '..'){
+        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+        }
+        $i = $i + 1;
+
+    }
+
+    /*
+    echo "<li>$list[0]</li>\n";
+    echo "<li>$list[1]</li>\n";
+    echo "<li>$list[2]</li>\n";
+    echo "<li>$list[3]</li>\n";
+    echo "<li>$list[4]</li>\n";
+    echo "<li>$list[5]</li>\n";
+    */
+}
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>
+        <?php
+        print_title();
+        ?>
+    </title>
 </head>
 <body>
     <?php
@@ -13,46 +56,16 @@
     <h1><a href="index.php">WEB</a></h1>
     <ol>
     <?php
-        $list = scandir('./data');
-
-        $i = 0;
-        while ($i < count($list)) {
-            if ($list[$i] != '.'){
-                if ($list[$i] != '..'){
-            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-                }
-            }
-            $i = $i + 1;
-
-        }
-
-        /*
-        echo "<li>$list[0]</li>\n";
-        echo "<li>$list[1]</li>\n";
-        echo "<li>$list[2]</li>\n";
-        echo "<li>$list[3]</li>\n";
-        echo "<li>$list[4]</li>\n";
-        echo "<li>$list[5]</li>\n";
-        */
+        print_list();
     ?>
     </ol>
     <h2>
         <?php
-        if (isset($_GET['id'])){
-            echo $_GET['id'];
-        } else {
-            echo "Welcom";
-        }
-
+        print_title();
         ?>
     </h2>
     <?php
-    if(isset($_GET['id'])){
-        echo file_get_contents("data/".$_GET['id']);
-    } else {
-        echo 'Hello, PHP';
-    }
-
+        print_description();
     ?>
 </body>
 </html>
